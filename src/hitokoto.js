9c180ad5884ac61525ed4484fc06f035402b5259
Wsine <weizhy3@hotmail.com>
2022-02-11 22:01:18 +0800

    update with parcel+pdfkit+chakraui

---
+++ src/hitokoto.js
@@ -0,0 +1,27 @@
+import dict_a from '../vendor/sentences-bundle/sentences/a.json'
+import dict_b from '../vendor/sentences-bundle/sentences/b.json'
+import dict_c from '../vendor/sentences-bundle/sentences/c.json'
+import dict_d from '../vendor/sentences-bundle/sentences/d.json'
+import dict_e from '../vendor/sentences-bundle/sentences/e.json'
+import dict_f from '../vendor/sentences-bundle/sentences/f.json'
+import dict_g from '../vendor/sentences-bundle/sentences/g.json'
+import dict_h from '../vendor/sentences-bundle/sentences/h.json'
+import dict_i from '../vendor/sentences-bundle/sentences/i.json'
+import dict_j from '../vendor/sentences-bundle/sentences/j.json'
+import dict_k from '../vendor/sentences-bundle/sentences/k.json'
+import dict_l from '../vendor/sentences-bundle/sentences/l.json'
+
+export default {
+  'a': dict_a,
+  'b': dict_b,
+  'c': dict_c,
+  'd': dict_d,
+  'e': dict_e,
+  'f': dict_f,
+  'g': dict_g,
+  'h': dict_h,
+  'i': dict_i,
+  'j': dict_j,
+  'k': dict_k,
+  'l': dict_l
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import fs from 'fs'
 import path from 'path'
 import ReactDOM from 'react-dom';
-import { ChakraProvider, Container, Heading } from '@chakra-ui/react'
+import { ChakraProvider, Container, Heading, Checkbox, Wrap, WrapItem, Button, Flex, Spacer } from '@chakra-ui/react'
+import hitokoto from './hitokoto.js'
 const PDFDocument = require('pdfkit').default
 const blobStream = require('blob-stream')
 const simheiFont = fs.readFileSync(path.join(__dirname, '../fonts/simhei.ttf'))
+
 
 async function generate_calendar() {
   // rounded off to 595 x 842 points(pt)
@@ -14,30 +16,50 @@ async function generate_calendar() {
   doc.registerFont('SimHei', simheiFont)
   const stream = doc.pipe(blobStream())
 
-  doc.lineWidth(5)
-    .lineJoin('round')
-    .rect(40, 40, 595 - 80, 842 - 80)
-    .stroke()
-  doc.lineWidth(3)
-    .rect(50, 50, 595 - 100, 842 - 100)
-    .stroke()
+  const yearHitokoto = get_hitokoto_sentences()
 
-  doc.moveDown(8)
-  doc.font('SimHei')
-    .fontSize(60)
-    .text('2021 年 1 月', {align: 'center'})
+  const year = new Date().getFullYear()
+  const daysOfMonth = [
+    31, year % 4 == 0 ? 29 : 28, 31, 30,
+    31, 30, 31, 31, 30, 31, 30, 31]
 
-  doc.moveDown()
-  doc.font('SimHei')
-    .fontSize(100)
-    .text('29', {align: 'center'})
+  let counter = 0
+  for (let month = 1; month <= 12; month++) {
+    for (let day = 1; day <= daysOfMonth[month - 1]; day++) {
+      doc.moveTo(0, 0)
 
-  doc.fontSize(80).moveDown()
-  doc.font('SimHei')
-    .fontSize(30)
-    .text('@正弦定理')
-    .moveDown(0.5)
-    .text('怕什么真理无穷，进一寸有一寸的欢喜。')
+      doc.lineWidth(5)
+        .lineJoin('round')
+        .rect(40, 40, 595 - 80, 842 - 80)
+        .stroke()
+      doc.lineWidth(3)
+        .rect(50, 50, 595 - 100, 842 - 100)
+        .stroke()
+
+      doc.font('SimHei')
+        .fontSize(60)
+        .moveDown()
+        .text(`${year} 年 ${month} 月`, {align: 'center'})
+
+      doc.font('SimHei')
+        .fontSize(100)
+        .moveDown()
+        .text(`${day}`, {align: 'center'})
+
+      const htkt = yearHitokoto[counter++]
+      const from = htkt['from_who'] || htkt['from'] || 'unknown'
+      const content = (htkt['hitokoto'] || 'Nothing').slice(0, 85)
+
+      doc.font('SimHei')
+        .fontSize(30)
+        .moveDown(2)
+        .text(`@${from}`)
+        .moveDown(0.5)
+        .text(`${content}`)
+
+      if (month != 12 || day <=30) doc.addPage()
+    }
+  }
 
   doc.end()
   stream.on('finish', () => {
@@ -48,11 +70,53 @@ async function generate_calendar() {
   })
 }
 
+
+function get_hitokoto_sentences() {
+  const daysOfYear = new Date().getFullYear() % 4 == 0 ? 366 : 365
+  const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  const checkedValues =
+    Array.from(document.querySelectorAll('input[type=checkbox]:checked'))
+    .map((box) => box.value)
+
+  const yearHitokoto = []
+  while (yearHitokoto.length < daysOfYear) {
+    let category = checkedValues[getRandomInt(0, checkedValues.length - 1)]
+    let item = hitokoto[category][getRandomInt(0, hitokoto[category].length - 1)]
+    yearHitokoto.push(item)
+  }
+
+  return yearHitokoto
+}
+
+
 export function App() {
+  const categories = [
+    '动画', '漫画', '游戏', '文学', '原创', '来自网络',
+    '其他', '影视', '诗词', '网易云', '哲学', '抖机灵'
+  ]
   return (
     <ChakraProvider>
       <Container>
         <Heading mt={5} mb={5}>Kindle Calendar Generator</Heading>
+        <Wrap spacing='10px' mb={5}>
+          {categories.map((item, index) => {
+            return (
+              <WrapItem>
+                <Checkbox defaultChecked name={'cb' + index}
+                    value={String.fromCharCode(97 + index)}>
+                  {item}
+                </Checkbox>
+              </WrapItem>
+            )
+          })}
+        </Wrap>
+        <Flex mb={5}>
+          <Spacer />
+          <Button colorScheme='teal' onClick={generate_calendar}>生成</Button>
+        </Flex>
         <iframe style={{
           border: '1px solid black',
           width: '100%',
@@ -63,7 +127,6 @@ export function App() {
   )
 }
 
+
 const app = document.getElementById('app');
 ReactDOM.render(<App />, app);
-
-generate_calendar()
